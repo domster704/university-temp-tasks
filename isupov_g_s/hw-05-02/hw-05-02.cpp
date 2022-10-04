680cf10dd82c0be01970f894fbfb89378074209d
Linker4 <61056244+domster704@users.noreply.github.com>
2022-10-04 11:13:55 +0300

    remaking epsilon condition

--- isupov_g_s/hw-05-02/hw-05-02.cpp
+++ isupov_g_s/hw-05-02/hw-05-02.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 
-double accumulate(int &a, int &b, double &step, double &eps) {
-    auto expression = [](double x) -> double {
+std::array<double, 2> accumulate(int &a, int &b, double &step, double &eps) {
+    auto expression = [&](double x) -> double {
         double resForXOnInfinity = 0;
+        double temp;
+        double lastElem = 0;
         for (int i = 1; i < 100; i++) {
-            resForXOnInfinity += std::pow(x, i) * std::sin(i * std::numbers::pi / 4);
+            temp = std::pow(x, i) * std::sin(i * std::numbers::pi / 4);
+            if (std::abs(temp - lastElem) < eps && i % 4 != 0) {
+                break;
+            }
+            resForXOnInfinity += temp;
+            lastElem = temp;
         }
         return resForXOnInfinity;
     };
@@ -14,19 +22,18 @@ double accumulate(int &a, int &b, double &step, double &eps) {
         return x * std::sin(std::numbers::pi / 4) / (1 - 2 * x * std::cos(std::numbers::pi / 4));
     };
 
-    double res = 0;
+    double resA = 0;
+    double resR = 0;
     double x;
     for (int i = 0; i < (std::abs(a) + std::abs(b) / step); i++) {
         x = a + i * step;
         double temp = expression(x);
         double compareTemp = compareExpression(x);
         printf("%8.5f | %8.5f | %8.5f\n", x, temp, compareTemp);
-        if (std::abs(temp - compareTemp) > eps) {
-            return res;
-        }
-        res += temp;
+        resA += temp;
+        resR += compareTemp;
     }
-    return res;
+    return std::array<double, 2> {resA, resR};
 }
 
 int main() {
@@ -34,6 +41,7 @@ int main() {
     int b = 1;
     double step = 0.05;
     double eps = 10e-3;
-    double res = accumulate(a, b, step, eps);
-    printf("Sum: %.5f", res);
+    printf("%8s | %8s | %8s\n", "x", "s(x)", "f(x)");
+    std::array<double, 2> res = accumulate(a, b, step, eps);
+    printf("SumAnalytical: %.5f\tSumReal: %.5f", res[0], res[1]);
 }

47e98ed080e67079f777e42587775b2d5fc4dd79
Linker4 <61056244+domster704@users.noreply.github.com>
2022-12-06 12:02:03 +0300

    Update circleGame.cpp

--- isupov_g_s/circleGame.cpp
+++ isupov_g_s/circleGame.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 #include "Vector.hpp"
 
 int main() {
-    VectorDec2D r{3, 5};
-    VectorDec2D velocity{5, 2};
-    int m1 = 1, m2 = 1, G = 1;
-    VectorDec2D centerAcceleration = -1 * m1 * m2 * G * r / std::pow(getLen(r), 3);
-    std::cout << centerAcceleration;
-    double dt = 1e-1;
-    for (int i = 0; i < 10; i++){
-        velocity = velocity + centerAcceleration;
-        r = r + velocity * dt;
-        centerAcceleration = -1 * m1 * m2 * G * r / std::pow(getLen(r), 3);
-        std::cout << "r: " << r << "v: " << velocity << "a: " << centerAcceleration << "\n";
+    std::ofstream out ("../report.csv");
+    if (out.is_open()) {
+        out << "n;t;x;y;v.x;v.y;a.x;a.y\n";
     }
+
+    VectorDec2D r{10, 0};
+    VectorDec2D velocity{0, 2};
+    int m1 = 1, m2 = 1, G = 100;
+    VectorDec2D centerAcceleration = G * r / std::pow(getLen(r), 3);
+    double dt = 1e0;
+    for (int i = 0; i < 1000 * 1 / dt; i++) {
+        velocity = velocity - dt * dt * centerAcceleration / 2;
+        r = r + velocity * dt;
+        centerAcceleration = G * r / std::pow(getLen(r), 3);
+
+        if (out.is_open()) {
+            out << i << ";" << dt * i << ";" << r.x << ";" << r.y << ";" << velocity.x << ";" << velocity.y << ";"
+                << centerAcceleration.x << ";" << centerAcceleration.y << "\n";
+        }
+    }
+    out.close();
 }
